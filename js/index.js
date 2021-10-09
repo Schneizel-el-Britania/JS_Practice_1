f27f90a441a872fea748fb3708f089bfcbc59eb2
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-10-09 11:55:26 +0300

    Finished customConcat method

--- js/index.js
+++ js/index.js
@@ -5,7 +5,11 @@
  */
 const customConcat = function () {
   let resultArray = [];
-  let i = 0;
+  for (let i = 0; i < this.length; i++) {
+    resultArray[i] = this[i];
+  }
+
+  let i = resultArray.length;
   for (array of arguments) {
     for (let j = 0; j < array.length; j++) {
       resultArray[i++] = array[j];
@@ -15,5 +19,5 @@ const customConcat = function () {
 }
 Array.prototype.customConcat = customConcat;
 
-const concatArray = [].customConcat([0, 1], [2, 3, 5], [4, 5, 5]);
+const concatArray = [2, 4].customConcat([0, 1], [2, 3, 5], [4, 5, 5]);
 console.log(concatArray);

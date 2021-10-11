f41b15d627e29e7150bfd128c5fc0a6b2e92938f
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-10-11 18:28:46 +0300

    Add methods to MyArray Constructor

--- js/MyArray.js
+++ js/MyArray.js
@@ -60,13 +60,55 @@ function MyArrayProto() {
     return result;
   }
 
-  this.reverse = function () {
-      let reversedArray = new MyArray();
-      for (let j = this.length - 1; j >= 0; j--) {
-        reversedArray.push(this[j]);
-      }
-      return reversedArray;
+  const getArrValues = function (arr) {
+    let resultArray = new MyArray();
+    for (let i = 0; i < arr.length; i++) {
+      resultArray.push(arr[i]);
     }
+    return resultArray;
+  }
+  this.concat = function () {
+    let resultArray = getArrValues(this);
+    for (array of arguments) {
+      for (let i = 0; i < array.length; i++) {
+        resultArray.push(array[i]);
+      }
+    }
+    return resultArray;
+  }
+
+
+  const swap = function (array, first, last) {
+    const temp = array[first];
+    array[first] = array[last];
+    array[last] = temp;
+  }
+  this.reverse = function () {
+    let arrLastElem = this.length - 1;
+    for (let i = 0; i < Math.floor(arrLastElem / 2); i++) {
+      swap(this, i, arrLastElem - i);
+    }
+    return this;
+  }
+
+  this.unshift = function () {
+    for (let j = this.length + arguments.length - 1; j >= 0; j--) {
+      this[j] = this[j - arguments.length] ? this[j - arguments.length] : arguments[j];
+    }
+    return (this.length += arguments.length);
+  };
+
+  this.shift = function () {
+    if (!this.length) {
+      return;
+    }
+    const deletedItem = this[0];
+    delete this[0];
+    this.length--;
+
+    return deletedItem;
+  };
+
 }
 
 /* Prototype */
@@ -78,3 +120,11 @@ const doubles = myArray.map(function (num) {
 });
 console.log('map:', doubles);
 console.log('myArray reverse:', myArray.reverse());
+console.log('myArray concat:', myArray.concat(new MyArray(33, 55, 66), new MyArray(77, 88, 66)));
+
+const unshiftMyArray = new MyArray(1, 2, 3);
+console.log('unshift:', unshiftMyArray.unshift(4, 5, 6), unshiftMyArray);
+
+const shiftMyArray = new MyArray('afds', 'csass', 'hhrwww');
+console.log('shift:', shiftMyArray.shift(), shiftMyArray);
+console.log('shift:', new MyArray().shift());

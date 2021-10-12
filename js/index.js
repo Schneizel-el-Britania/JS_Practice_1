98579e7e2ddbb8528bdfbebb0419a7059da12e56
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-10-12 16:16:40 +0300

    Delete unnecessary data

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/* TASK 1 */
+const getPow = (num, exp) => {
+  if (exp === 1) {
+    return num;
+  }
+
+  const pow = (pow, exp) => num * pow(num, exp - 1);
+  return exp > 0 ? pow(getPow, exp) : (1 / pow(getPow, Math.abs(exp)));
+}
+
+console.log(getPow(2, -3));
+
+
+/* TASK 2 */
+const getPairBrackets = function (count) {
+  const baskets = '()';
+
+  if (count <= 1) {
+    return baskets;
+  }
+  return baskets.substring(0, baskets.length / 2) + getPairBrackets(count - 1) + baskets.substring(baskets.length / 2, baskets.length);
+}
+
+console.log(getPairBrackets(-1));
+console.log(getPairBrackets(3));
+
+
+/* TASK 3 */
+const getMinMaxValue = (array) => [Math.min(...array), Math.max(...array)];
+console.log(getMinMaxValue([5, 1, 23, 3, -4, 0]));
+
+
+/* TASK 4 */
+const mulNums = (...args) => args.reduce((res, num) => res * num);
+console.log(mulNums(1, 2, 3, 4, 5));

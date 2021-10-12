805a464c85761ac8d7d192383ff78cd329745f6c
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-10-12 20:05:53 +0300

    Add fun version

--- js/index.js
+++ js/index.js
@@ -4,4 +4,7 @@ const getVowelsCount = function (str) {
   return [...str].reduce((res, char) => res + vowels.some((item) => char.toLowerCase().includes(item)), 0);
 }
 
+const getVowelsCountFun = (str) => [...str].reduce((res, char) => res + ['a', 'u', 'e', 'i', 'o'].some((item) => char.toLowerCase().includes(item)), 0);
+
 console.log(getVowelsCount('aafUfksOEEE'));
+console.log(getVowelsCountFun('aafUfksOEEE'));

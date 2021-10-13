a34f1c144fbd6506a6319a36bca28a7a72fc6235
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-10-13 22:43:25 +0300

    Add RangeValidator class

--- js/index.js
+++ js/index.js
@@ -1,10 +1,41 @@
-/* TASK 1 */
-const getVowelsCount = function (str) {
-  const vowels = ['a', 'u', 'e', 'i', 'o'];
-  return [...str].reduce((res, char) => res + vowels.some((item) => char.toLowerCase().includes(item)), 0);
+class RangeValidator {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  _checkData(value, type) {
+    if (typeof value !== type) {
+      throw new RangeError(`Value must be ${type}`);
+    }
+  }
+
+  set from(from) {
+    this._checkData(from, 'number');
+    this._from = from;
+  }
+  set to(to) {
+    this._checkData(to, 'number');
+    this._to = to;
+  }
+
+  get from() { return this._from; }
+  get to() { return this._to; }
+
+  get range() { return [this._from, this._to]; }
+
+  validate(num) {
+    if (num >= this._from && num <= this._to) {
+      return num;
+    } else {
+      throw new RangeError('Number must be in range')
+    }
+  }
+
 }
 
-const getVowelsCountFun = (str) => [...str].reduce((res, char) => res + ['a', 'u', 'e', 'i', 'o'].some((item) => char.toLowerCase().includes(item)), 0);
 
-console.log(getVowelsCount('aafUfksOEEE'));
-console.log(getVowelsCountFun('aafUfksOEEE'));
+const numberRange = new RangeValidator(1, 13);
+console.log(numberRange.range);
+console.log(numberRange.validate(13));
+console.log(numberRange.validate(14));

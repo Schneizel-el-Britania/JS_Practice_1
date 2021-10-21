7858d63ab71146be1366841dc30b0c2aded09ac4
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-10-21 17:32:42 +0300

    Add figures classes

--- js/index.js
+++ js/index.js
@@ -1,41 +1,72 @@
-class RangeValidator {
-  constructor(from, to) {
-    this.from = from;
-    this.to = to;
+class Figure3D {
+  constructor(name) {
+    this.name = name;
   }
 
-  _checkData(value, type) {
+  _checkValue(value, type) {
     if (typeof value !== type) {
-      throw new RangeError(`Value must be ${type}`);
+      throw TypeError('Value should be ' + type);
+    } else if (value <= 0) {
+      throw RangeError('Value should be more than 0');
     }
+
+    return true;
   }
 
-  set from(from) {
-    this._checkData(from, 'number');
-    this._from = from;
-  }
-  set to(to) {
-    this._checkData(to, 'number');
-    this._to = to;
-  }
-
-  get from() { return this._from; }
-  get to() { return this._to; }
-
-  get range() { return [this._from, this._to]; }
-
-  validate(num) {
-    if (num >= this._from && num <= this._to) {
-      return num;
-    } else {
-      throw new RangeError('Number must be in range')
-    }
-  }
-
+  get figureVolume() { }
 }
 
 
-const numberRange = new RangeValidator(1, 13);
-console.log(numberRange.range);
-console.log(numberRange.validate(13));
-console.log(numberRange.validate(14));
+class Sphere extends Figure3D {
+  constructor(r) {
+    super('Sphere');
+
+    if (this._checkValue(r, 'number')) {
+      this.r = r;
+    }
+  }
+
+  get figureVolume() {
+    return (4 * Math.PI * this.r ** 3) / 3;
+  }
+}
+
+class Cube extends Figure3D {
+  constructor(a) {
+    super('Cube');
+
+    if (this._checkValue(a, 'number')) {
+      this.a = a;
+    }
+  }
+
+  get figureVolume() {
+    return this.a ** 3;
+  }
+}
+
+class Cylinder extends Figure3D {
+  constructor(r, h) {
+    super('Cylinder');
+
+    if (this._checkValue(r, 'number')) {
+      this.r = r;
+    }
+    if (this._checkValue(h, 'number')) {
+      this.h = h;
+    }
+  }
+
+  get figureVolume() {
+    return Math.PI * this.r ** 2 * this.h;
+  }
+}
+
+const getFigureVolume = function (figureName, figureType, ...values) {
+  figureName = new figureType(...values);
+  console.log(figureName.name, figureName.figureVolume);
+}
+
+getFigureVolume('sphere', Sphere, 10);
+getFigureVolume('cube', Cube, 15);
+getFigureVolume('cylinder', Cylinder, 5, 50);
